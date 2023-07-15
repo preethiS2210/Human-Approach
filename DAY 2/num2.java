f3c9a6e7e184aa1ac270eeebc8556f4d516353e1
preethiS2210 <139195145+preethiS2210@users.noreply.github.com>
2023-07-15 15:39:51 +0530

    Add files via upload

---
+++ DAY 2/num2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class num2
+{
+	public static void main(String[] args)
+	{
+		int n =5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(j);
+			}
+			
+				System.out.println();
+		}
+	}
+}
